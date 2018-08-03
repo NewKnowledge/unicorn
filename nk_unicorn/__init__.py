ad32009174f6780ab49a284d1ee7131935bb7773
Ubuntu <becky@Becky-gpu.i2kqjbats1welhdlmn4k4ihr2e.jx.internal.cloudapp.net>
2018-08-03 18:08:59 +0000

    changes to init for new files

--- nk_unicorn/__init__.py
+++ nk_unicorn/__init__.py
@@ -1,7 +1,9 @@
 from .unicorn import Unicorn
+from .dim_reduction import SpikesortTSNE
+from .clustering import HDBSCAN, FastAgglomerative
 
 __version__ = '1.0.0'
 
 __all__ = [
-    'Unicorn'
-]
+    'Unicorn', 'SpikesortTSNE', 'HDBSCAN', 'FastAgglomerative'
+    ]
