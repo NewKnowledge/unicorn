abbf197815b7b3d1c213c754cc8a11666136a48c
Craig Corcoran <craig@newknowledge.io>
2018-12-03 12:53:18 -0600

    add umap and hdbscan config to tests

--- tests.py
+++ tests.py
@@ -6,14 +6,29 @@ import numpy as np
 config_logger(prefix="unicorn", root_log_level="ERROR", formatter=Formatter())
 logger = get_logger(__name__)
 
+dim = 100
+n_samples = 1000
+data = np.random.randn(n_samples, dim)
 
-def test_unicorn(n_samples=1000, dim=100):
+
+def test_basic():
     unicorn = Unicorn()
-    data = np.random.randn(n_samples, dim)
     res = unicorn.cluster(data)
     assert isinstance(res, np.ndarray)
     assert res.shape == (n_samples,)
+    assert isinstance(res[0], (np.int32, np.int64))
+
+
+def test_algs():
+    unicorn = Unicorn(dim_reduc_alg="umap", cluster_alg="hdbscan")
+    res = unicorn.cluster(data)
+    assert isinstance(res, np.ndarray)
+    assert res.shape == (n_samples,)
+    assert isinstance(res[0], (np.int32, np.int64))
+
+
+# TODO add tests for algs other than defaults
 
 
 if __name__ == "__main__":
-    test_unicorn()
+    test_basic()
